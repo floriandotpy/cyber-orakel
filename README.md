8b845af599f6ee6fef2f37abf2b159c1bf819489
Florian <florian@casualcoding.com>
2024-12-16 21:43:52 +0100

    Add local llm, add text to speech

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ Erfahre deine Zukunft per Cyber-Keks
 
 # Setup
 
+Install Ollama (needed for running a local LLM)
+
+- https://ollama.com/download/
+
+Fetch the following LLM. It's small and should run on current local machines all the way down to a Raspberry Pi 5.
+
+```commandline
+ollama pull gemma2:2b
+
+# optional: try if the model runs properly
+ollama run gemma2:2b
+```
+
+
 ```commandline
 python3 -m venv venv
 pip install -r requirements.txt
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from typing import Optional
+
+import pygame
+from fastapi import FastAPI
+from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from typing import Optional
+from gtts import gTTS
+from langchain_ollama import ChatOllama
 
 app = FastAPI()
 
@@ -15,13 +20,37 @@ app.add_middleware(
 )
 
 
+def read_out_loud(text: str, lang: str = "de") -> str:
+    tts = gTTS(text=text, lang=lang, slow=False)
+    tts.save("speech.mp3")
+
+    # play message
+    pygame.mixer.init()
+    pygame.mixer.music.load("speech.mp3")
+    pygame.mixer.music.play()
+
+
+def generate_fortune(zodiac: str, sentiment: str, num_lines: int = 2, language: str = "German") -> str:
+    prompt = f"""You are a fortune teller in a cyberpunk story.
+Write a fortune cookie message for the cyber zodiac "{zodiac}" 
+with a sentiment of "{sentiment}". The message should be exactly {num_lines} lines long.
+Write in {language}. Do not explain your answer. Be short and concise."""
+
+    llm = ChatOllama(model="gemma2:2b")
+    msg = llm.invoke(prompt)
+
+    return msg.content
+
+
 @app.get("/fortune")
 def fortune(zodiac: Optional[str] = None, sentiment: Optional[str] = None):
     if not zodiac or not sentiment:
         raise HTTPException(status_code=400, detail="Missing parameters")
 
     # Generate a fortune cookie text based on the parameters
-    fortune_text = f"Your {zodiac} sign indicates a {sentiment} day ahead!"
+    fortune_text = generate_fortune(zodiac, sentiment)
+
+    read_out_loud(fortune_text)
 
     return {"fortune": fortune_text}
 
