f5d1a7646f43b0d8aadc19e90f4b2ebba23bdea4
Florian <florian@casualcoding.com>
2024-12-24 17:01:08 +0100

    Extend prompt based on zodiac for more varied generation

---
+++ cyber_orakel/fortune.py
@@ -0,0 +1,153 @@
+import random
+from dataclasses import dataclass
+
+from langchain_ollama import ChatOllama
+
+
+@dataclass
+class CyberZodiac:
+    key: str
+    display_name: str
+    prompt_snippet: str
+
+
+SENTIMENTS = ["positive", "ecstatic", "optimistic", "dismal", "neutral", "mythical"]
+ZODIAC_SIGNS: list[CyberZodiac] = [
+    CyberZodiac(
+        key="cyber_krieger",
+        display_name="Cyber-Krieger",
+        prompt_snippet="""
+        - Cyber-Implantat
+        - Hacking
+        - Künstliche Intelligenz
+        - Virtuelle Realität
+        - Cyberspace"""),
+    CyberZodiac(
+        key="wissensanarcho",
+        display_name="Wissensanarcho",
+        prompt_snippet="""
+        - Open Source
+        - Datenschutz
+        - Verschlüsselung
+        - Whistleblowing
+        - Anonymität
+        - Hackerethik
+        - Wissen ist Macht
+        - Wissen teilen
+        - Wissen ist frei
+"""),
+    CyberZodiac(
+        key="einhorn",
+        display_name="Einhorn",
+        prompt_snippet="""
+        - Regenbogen
+        - Glitzer
+        - Einhörner
+        - Magie
+        - Liebe
+        - Freundschaft
+        - Konfetti
+        - Kreativität
+        - Chaos ist schön
+"""),
+    CyberZodiac(
+        key="cryptogeek",
+        display_name="Cryptogeek",
+        prompt_snippet="""
+        - Public Key
+        - Private Key
+        - Alles verschlüsseln
+        - Blockchain
+        - Keysigning Party
+        - GPG Key
+        - https everywhere
+        """),
+    CyberZodiac(
+        key="codeglaeubig",
+        display_name="Codegläubig",
+        prompt_snippet="""
+        - Code ist Gesetz
+        - Alles ist 1 außer der 0
+        - Code ist Poesie
+        - Code ist Kunst
+        """),
+    CyberZodiac(
+        key="schwurbler",
+        display_name="Schwurbler",
+        prompt_snippet="""
+        - Chemtrails
+        - Die Erde ist eine Scheibe
+        - 5G
+        - Globuli
+        - Aluhut
+        - Aluburka
+        """),
+    CyberZodiac(
+        key="retrohacker",
+        display_name="Retrohacker",
+        prompt_snippet="""
+        - 8-Bit
+        - C64
+        - Floppy Disk
+        - Retro
+        - Hacker Manifest
+        - Funkausstellung
+        - BTX
+        """),
+    CyberZodiac(
+        key="datenelch",
+        display_name="Datenelch",
+        prompt_snippet="""
+        - Wlan Geweih
+        - Empfangs-Turbo
+        - High speed Internet
+        - Wald
+        - Wald und Wiesen Internet
+        - Datenautobahn
+        """),
+    CyberZodiac(
+        key="tschunky",
+        display_name="Tschunky",
+        prompt_snippet="""
+        - Tschunk ist Liebe
+        - Mate macht wach
+        - Limetten sind die neuen Zitronen
+        - Tschunk ist Leben
+        - Um 3 an der Tschunk-Bar
+        - Tschunk ist die Antwort
+        """)
+]
+
+
+def generate_fortune(zodiac_key: str, sentiment: str, num_lines: int = 2, language: str = "German") -> str:
+    zodiac = next((z for z in ZODIAC_SIGNS if z.key == zodiac_key), None)
+    if not zodiac:
+        # invalid zodiac sign, pick a random one
+        print(f"Invalid zodiac sign: {zodiac_key}, picking a random one")
+        zodiac = random.choice(ZODIAC_SIGNS)
+
+    prompt = f"""You are a fortune teller in a cyberpunk story.
+    Write a fortune cookie message for the cyber zodiac "{zodiac.display_name}"
+    with a sentiment of "{sentiment}". The message should be exactly {num_lines} lines long.
+    Write in {language}. Do not explain your answer. Be short and concise. Add no special characters.
+    The following terms and phrases are typical for the cyber zodiac {zodiac.display_name}. 
+    Use them as inspiration for the message but don't just copy them verbatim: 
+    {zodiac.prompt_snippet}"""
+
+    llm = ChatOllama(model="gemma2:2b")
+    msg = llm.invoke(prompt)
+
+    return msg.content
+
+
+def generate_many_fortunes():
+    for zodiac in ZODIAC_SIGNS:
+        for sentiment in SENTIMENTS:
+            for _ in range(2):
+                print(f"Zodiac: {zodiac.key}, Sentiment: {sentiment}")
+                print(generate_fortune(zodiac.key, sentiment))
+            print()
+
+
+if __name__ == '__main__':
+    generate_many_fortunes()
--- cyber_orakel/server.py
+++ cyber_orakel/server.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from typing import Optional
 
@@ -6,62 +7,17 @@ from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from langchain_ollama import ChatOllama
 from starlette.responses import FileResponse
 
+from cyber_orakel.fortune import SENTIMENTS
+from cyber_orakel.fortune import ZODIAC_SIGNS
+from cyber_orakel.fortune import generate_fortune
 from cyber_orakel.print import print_receipt
 
 
 @dataclass
 class Settings:
     enable_printer: bool = True
-
-
-@dataclass
-class CyberZodiac:
-    key: str
-    display_name: str
-    prompt_snippet: str
-
-
-ZODIAC_SIGNS: list[CyberZodiac] = [
-    CyberZodiac(
-        key="cyber_krieger",
-        display_name="Cyber-Krieger",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="wissensanarcho",
-        display_name="Wissensanarcho",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="einhorn",
-        display_name="Einhorn",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="cryptogeek",
-        display_name="Cryptogeek",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="codeglaeubig",
-        display_name="Codegläubig",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="schwurbler",
-        display_name="Schwurbler",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="retrohacker",
-        display_name="Retrohacker",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="datenelch",
-        display_name="Datenelch",
-        prompt_snippet=""),
-    CyberZodiac(
-        key="tschunky",
-        display_name="Tschunky",
-        prompt_snippet="")
-]
 
 
 class CyberOracleServer:
@@ -80,25 +36,16 @@ class CyberOracleServer:
             allow_headers=["*"],
         )
 
-    def generate_fortune(self, zodiac: str, sentiment: str, num_lines: int = 2, language: str = "German") -> str:
-        prompt = f"""You are a fortune teller in a cyberpunk story.
-        Write a fortune cookie message for the cyber zodiac "{zodiac}"
-        with a sentiment of "{sentiment}". The message should be exactly {num_lines} lines long.
-        Write in {language}. Do not explain your answer. Be short and concise."""
-
-        llm = ChatOllama(model="gemma2:2b")
-        msg = llm.invoke(prompt)
-
-        return msg.content
-
     def setup_routes(self):
         @self.app.get("/fortune")
         def fortune(zodiac: Optional[str] = None, sentiment: Optional[str] = None):
             if not zodiac or not sentiment:
                 raise HTTPException(status_code=400, detail="Missing parameters")
 
+            sentiment = random.choice(SENTIMENTS) if sentiment == "random" else sentiment
+
             # Generate a fortune cookie text based on the parameters
-            fortune_text = self.generate_fortune(zodiac, sentiment)
+            fortune_text = generate_fortune(zodiac, sentiment)
 
             if self.settings.enable_printer:
                 print_receipt(fortune_text)
